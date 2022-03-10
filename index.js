82732aced1c17526b74707fdca0284a29999dd2e
Munya-sya <kennedymunyasya2017@gmail.com>
2022-03-10 11:36:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+app.set('views','views');
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    response.render('home');
+});
+
+app.listen(port);
+console.log('Node server started on port 3000');
