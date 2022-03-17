0aa70283808e7a9025101e5bf2a32527c4cd5be8
Munya-sya <kennedymunyasya2017@gmail.com>
2022-03-17 13:12:23 +0300

    modified code

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require('fs');
+const fileName = 'bmi.json';
 
 const bodyParser = require('body-parser');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+// Load data from file
+let rawData = fs.readFileSync(fileName);
+let data = JSON.parse(rawData);
 
 app.set('views','views');
 app.set('view engine', 'hbs');
@@ -11,6 +17,27 @@ app.use(express.static('public'));
 
 app.get('/', function(request, response){
     response.render('home');
+});
+app.get('/process-data', function (request, response) {
+    data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    response.send(data);
+});
+
+app.post('/process-data', urlEncodedParser, function(request, response){
+    const fs = require('fs');
+    const path = require('path');
+
+    let info = 
+            {
+                  weight: request.body.weight,
+                  height: request.body.height,
+                  BMI: request.body.BMI,
+                  Summary: request.body.diagnosis
+            };
+            data.push(info);
+            fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+                  
+            response.render('results', info);
 });
 
 app.listen(port);
